db203d517608927bf360e3c46f2a32d294c74406
sarah melton <sarah.melton1@gmail.com>
2022-02-09 22:28:49 -0500

    Initial commit

---
+++ books.js
@@ -0,0 +1,13 @@
+//I chose the name books.js to indicate that the file is about books.
+//This variable stores the number of books checked out
+var checkedOut = 74;
+//This variable stores the total number of books
+var numberOfBooks = 985;
+//This variable stores the name of the Library
+var libraryName = "Brighton Branch"
+//This variable checks to see if the book is in a digital format
+var isDigital = true;
+var isCheckedOut = false;
+var libraryMascot = "cat";
+
+var childrensBooks = ["Goodnight Moon", "Cat in the Hat", "Where the Wild Things Are"];
---
+++ challenge/index.md
@@ -0,0 +1,9 @@
+# Book Titles
+
+* A Brief History of 7 Killings
+* Amber Revolution
+* Learn to Code by Solving Problems
+* Harry Potter and the Chamber of Secrets
+* Harry Potter and the Goblet of Fire
+
+We both "drove," which may not have been the most efficient way to approach the problem! We got stuck on cloning the repository, but after that it moved pretty quickly. We also got a little stuck coming up with content.
